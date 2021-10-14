f0bd7115654f1c25cc03a308c2dce1b796358cdb
DasithDeelaka <deelakajagoda@gmail.com>
2021-10-14 14:12:38 +0530

    Task completion status update route implemented

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -75,4 +75,43 @@ todoRouter.post("/create", auth, async (req, res) => {
     }
 });
 
+todoRouter.post("/update", auth, async (req, res) => {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+        const id = req.body._id;
+
+        if (!username || !password || !id) {
+            return res.status(400).json({ msg: "Not all the required fields have been entered!" });
+        } else {
+            const existingUser = await User.findOne({ username: username });
+
+            if (!existingUser) {
+                return res
+                .status(400)
+                .json({ msg: "An account with this username does not exists!" });
+            } else {
+                const passwordMatch = await bcrypt.compare(password, existingUser.password);
+
+                if (!passwordMatch) {
+                    return res
+                    .status(400)
+                    .json({ msg: "Not Authorized!" });
+                } else {
+                    Todo.findByIdAndUpdate(id)
+                    .then((todo) => {
+                        todo.completion = 1;
+                        todo.save()
+                        .then(() => res.json('Successfully Marked As Completed!'))
+                        .catch(err => res.status(400).json(err));
+                    })
+                    .catch(err => res.status(400).json(err));
+                }
+            }
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 module.exports = todoRouter;
